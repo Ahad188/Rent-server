7ef83afeddeaf0d1c5952476d3e996070b12d70d
AhaD <abuahad35188@gmail.com>
2023-09-11 23:51:25 +0600

    vercel add

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const app = express();
 const port = process.env.PORT ||5000;
 
 const corsConfig = {
-     origin:'*', 
-     credentials:true,
-     optionSuccessStatus:200,
+     origin: '',
+     credentials: true,
+     methods: ['GET', 'POST', 'PUT', 'DELETE']
  }
-
-
-app.use(cors(corsConfig))
+ app.use(cors(corsConfig))
+ app.options("", cors(corsConfig))
 // app.options("", cors(corsConfig))
 app.use(express.json())
  
